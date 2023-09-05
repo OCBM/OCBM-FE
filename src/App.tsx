4bd9d99ff055ae66951fccb03d01b3e33b359bd2
Manoj Surabathulla <manojsurabathulla@Manoj-Surabathulla-2.local>
2023-09-05 15:02:34 +0530

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,14 @@
 import { Route } from 'react-router-dom';
 import Home from './pages/Home';
 import { PublicRoutes } from './routes';
-import Dropdown from './components/dropdown';
 
 function App() {
-  const dataOptions = [
-    {
-      id: 1,
-      text: 'Alert Data',
-    },
-    {
-      id: 2,
-      text: 'Warn Data',
-    },
-  ];
   return (
     <div>
       <PublicRoutes>
         <Route path="/" element={<Home />} />
         <Route path="*" element={<h1>Not found</h1>} />
       </PublicRoutes>
-      <Dropdown
-        options={dataOptions}
-        handleChange={function (val: string): void {
-          throw new Error('Function not implemented.');
-        }}
-      />
-      <Dropdown
-        type="secondary"
-        options={dataOptions}
-        handleChange={function (val: string): void {
-          throw new Error('Function not implemented.');
-        }}
-      />
     </div>
   );
 }
