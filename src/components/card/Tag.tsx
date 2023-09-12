e421ae1865510eee37609e7523cc02c4bd9094f4
Raja rathinam v <raja.rathinam@flyerssoft.com>
2023-09-12 15:51:15 +0530

    Tag component class updated

--- src/components/card/Tag.tsx
+++ src/components/card/Tag.tsx
@@ -3,7 +3,9 @@ const Tag = ({ type }: any) => {
     type === 'high' ? 'bg-red-600' : type === 'medium' ? 'bg-yellow-600' : 'bg-green-600';
 
   return (
-    <span className={`${TAG_COLOR} text-white pt-1 pb-4 rounded-t-[5px] px-3 -z-50 relative`}>
+    <span
+      className={`${TAG_COLOR} text-white pt-1 pb-4 rounded-t-[5px] px-3 -z-50 relative capitalize`}
+    >
       {type}
     </span>
   );
