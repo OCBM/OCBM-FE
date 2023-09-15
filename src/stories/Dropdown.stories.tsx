4c7be91c078c8b4230a5b65b66dc1fc43db0aad1
manoj <manoj.surabathulla@flyerssoft.com>
2023-09-15 19:25:28 +0530

    updated tsconfig file

--- src/stories/Dropdown.stories.tsx
+++ src/stories/Dropdown.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import Dropdown from '@/components/dropdown';
+import Dropdown from 'components/dropdown';
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta = {
