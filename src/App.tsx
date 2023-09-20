a01067ad5be82d3f9b65a6e88e66f4b2622818cf
Roslind <roslind.vasantha@flyerssoft.com>
2023-09-20 14:36:48 +0530

    review comments fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,15 @@
-// import { Route } from 'react-router-dom';
-// import Home from './pages/Home';
-// import { PublicRoutes } from './routes';
-// import './App.css';
-import { useState } from 'react';
-
-import FileUploader from 'components/fileuploader';
+import { Route } from 'react-router-dom';
+import Home from './pages/Home';
+import { PublicRoutes } from './routes';
+import './App.css';
 
 function App() {
-  const [status, setStatus] = useState('upload');
-
-  const handleFile = (event) => {
-    setStatus('success');
-  };
-
   return (
     <div>
-      {/* <PublicRoutes> */}
-      {/* <Route path="/" element={<Home />} /> */}
-      {/* <Route path="*" element={<h1>Not found</h1>} /> */}
-      {/* </PublicRoutes> */}
-      <FileUploader handleFile={handleFile} uploadStatus={status} />
+      <PublicRoutes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<h1>Not found</h1>} />
+      </PublicRoutes>
     </div>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src/'),
       components: `${path.resolve(__dirname, './src/components/')}`,
-      assets: `${path.resolve(__dirname, './src/assets/')}`,
+      assets: path.resolve(__dirname, './src/assets/'),
       public: `${path.resolve(__dirname, './public/')}`,
       pages: path.resolve(__dirname, './src/pages'),
     },
