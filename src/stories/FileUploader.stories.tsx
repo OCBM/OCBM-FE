281da60e24f1fae1cdb34ee10056155a89c533d4
Roslind <roslind.vasantha@flyerssoft.com>
2023-09-25 18:47:34 +0530

    created UI and functionality stories

--- src/stories/FileUploader.stories.tsx
+++ src/stories/FileUploader.stories.tsx
@@ -1,12 +1,14 @@
+import { useState } from 'react';
 import { Meta, StoryObj } from '@storybook/react';
 import FileUploader from '../components/fileuploader';
+import { FileUploadStatusType } from 'components/fileuploader/types';
 
 const meta = {
   title: 'Components/FileUploader',
   component: FileUploader,
   argTypes: {
     fileFormat: {
-      options: ['.xlsx'],
+      options: ['.xlsx', '.png', '.pdf', '.jpg', 'jpeg'],
       control: { type: 'radio' },
     },
     uploadStatus: {
@@ -20,4 +22,17 @@ export default meta;
 
 type Story = StoryObj<typeof FileUploader>;
 
-export const Default: Story = {};
+const FileUploaderWithHooks = () => {
+  const [uploadStatus, setUploadStatus] = useState<FileUploadStatusType>('upload');
+
+  const handleFile = () => {
+    setUploadStatus('success');
+  };
+  return <FileUploader fileFormat=".xlsx" uploadStatus={uploadStatus} handleFile={handleFile} />;
+};
+
+export const UI: Story = {};
+
+export const Functionality: Story = {
+  render: () => <FileUploaderWithHooks />,
+};
