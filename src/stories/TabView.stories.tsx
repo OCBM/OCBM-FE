7b8b914fa91c84ad0d3021d0fd137837d3513fcc
Roslind <roslind.vasantha@flyerssoft.com>
2023-09-26 14:59:30 +0530

    updated the primary type

--- src/stories/TabView.stories.tsx
+++ src/stories/TabView.stories.tsx
@@ -1,12 +1,19 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import TabView from 'components/tabview';
 
-const primaryTabs = [{ title: 'title', key: 'key' }];
-const secondaryTabs = [{ title: 'title', key: 'key' }];
+const primaryTabs = [
+  { title: 'Plant', key: 'plant', content: 'content' },
+  { title: 'Shop', key: 'shop', content: 'content' },
+];
+const secondaryTabs = [
+  { title: 'Machines', key: 'machines', path: '/machines' },
+  { title: 'Sensors', key: 'sensors', path: '/sensors' },
+];
 
 const meta = {
   title: 'components/TabView',
   component: TabView,
+
   parameters: {
     layout: 'centered',
   },
