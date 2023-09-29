ec90c922c5642487fe019a7c7db4295b3b7164ed
Raja rathinam v <raja.rathinam@flyerssoft.com>
2023-09-29 19:20:31 +0530

    Errorfallback page updated

--- src/components/error/ErrorFallback.tsx
+++ src/components/error/ErrorFallback.tsx
@@ -1,12 +1,8 @@
-import Layout from 'components/layout';
-
 function ErrorFallback() {
   return (
-    <Layout>
-      <div className="h-[100vh] flex items-center justify-center">
-        <h2 className="text-black text-[20px]">Something went wrong</h2>
-      </div>
-    </Layout>
+    <div className="h-[100vh] flex items-center justify-center">
+      <h2 className="text-black text-[20px]">Something went wrong</h2>
+    </div>
   );
 }
 export default ErrorFallback;
