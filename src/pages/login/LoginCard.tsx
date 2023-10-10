af296a5cf5aba0a1b06f54739fcb7ec61252f54d
manoj <manoj.surabathulla@flyerssoft.com>
2023-10-10 13:51:56 +0530

    added login flow to reset password flow

--- src/pages/login/LoginCard.tsx
+++ src/pages/login/LoginCard.tsx
@@ -1,36 +1,36 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import classNames from 'classnames';
 import { Logo } from '@/assets/images';
 import { Button, Input } from '@/components';
 import LoginLayout from './layout';
 import ResetPassword from './ResetPassword';
-import { loginUser } from '@/redux/slices/authSlice';
 import { useAppDispatch, useAppSelector } from '@/hooks';
 import { RootState } from '@/redux/store';
 import { toast } from 'react-toastify';
 import { SITEMAP } from '@/utils/sitemap';
+import { loginUser } from '@/redux/slices/authSlice';
 
 const LoginCard = () => {
-  const { user } = useAppSelector((store: RootState) => store?.auth);
-  const dispatch = useAppDispatch();
-  const navigate = useNavigate();
-  const [showResetPassword, setShowResetPassword] = useState(false);
+  const [showForgotPassword, setShowForgotPassword] = useState<boolean>(false);
   const [formData, setFormData] = useState({
     username: '',
     password: '',
     newPassword: '',
-    conformPassword: '',
+    confirmPassword: '',
+    reset: false,
   });
+
+  const { user } = useAppSelector((store: RootState) => store?.auth) || {};
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (user) {
       navigate(SITEMAP.base.index, { replace: true });
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [user]);
-
-  const resetPasswordClick = () => {
-    setShowResetPassword(!showResetPassword);
-  };
 
   const inputHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
     const name = event.target.name;
@@ -41,16 +41,41 @@ const LoginCard = () => {
   const loginSubmit = () => {
     const { username, password } = formData;
     if (!username || !password) {
-      toast.error('provide all fields');
+      toast.warn('Provide All Fields');
       return;
     }
     dispatch(loginUser({ username, password }));
   };
 
+  const handleForgotPassword = (val: boolean) => {
+    if (!formData?.reset === val) {
+      setShowForgotPassword(!showForgotPassword);
+      setFormData((prevState) => ({ ...prevState, reset: true }));
+    } else {
+      setFormData((prevState) => ({ ...prevState, reset: false }));
+    }
+  };
+
+  const forgotPasswordBack = (val: boolean) => {
+    if (!formData?.reset === val) {
+      setFormData((prevState) => ({ ...prevState, reset: true }));
+    } else {
+      setShowForgotPassword(!showForgotPassword);
+    }
+  };
+
+  const loginBtnClass = classNames(`w-full
+  ${formData?.username.length < 6 || formData?.password.length < 8 ? 'cursor-not-allowed' : 'cursor-pointer'}`);
+
   return (
     <LoginLayout>
-      {showResetPassword ? (
-        <ResetPassword onChange={inputHandler} formData={formData} />
+      {showForgotPassword ? (
+        <ResetPassword
+          onChange={inputHandler}
+          formData={formData}
+          handleForgotPassword={handleForgotPassword}
+          forgotPasswordBack={forgotPasswordBack}
+        />
       ) : (
         <div className="flex flex-col items-center">
           <div className=" h-fit w-[380px] rounded-3xl border bg-white">
@@ -59,13 +84,13 @@ const LoginCard = () => {
                 <Logo className="w-full" />
                 <div className="w-full">
                   <div className="flex justify-center items-center flex-col">
-                    <p className="font-GothamMedium text-base font-medium italic text-[#000] pt-[50px]">Login</p>
+                    <p className="font-GothamMedium text-base font-medium italic text-black pt-[50px]">Login</p>
                     <div className="pt-10 w-full">
                       <Input
                         name="username"
                         value={formData.username}
                         placeholder="User Name*"
-                        className="py-7 pl-7 border border-grey-dark text-grey-light h-[40px]"
+                        className="p-5 border border-grey-dark text-grey-light"
                         onChange={inputHandler}
                       />
                     </div>
@@ -75,23 +100,18 @@ const LoginCard = () => {
                         name="password"
                         value={formData.password}
                         placeholder="Password*"
-                        className="py-7 pl-7 border border-grey-dark text-grey-light h-[40px]"
+                        className="p-5 border border-grey-dark text-grey-light"
                         onChange={inputHandler}
                       />
                     </div>
                     <p
-                      className="py-5 font-medium text-base text-[#5F2EEA] cursor-pointer"
-                      onClick={resetPasswordClick}
+                      className="py-5 font-medium text-base text-textThemeColor cursor-pointer"
+                      onClick={() => handleForgotPassword(true)}
                     >
                       <i>Forgot Password</i>
                     </p>
                     <div className="w-full">
-                      <Button
-                        disabled={formData?.username.length < 6 || formData?.password.length < 6 ? true : false}
-                        label="Log In"
-                        className="w-full cursor-pointer"
-                        onClick={loginSubmit}
-                      />
+                      <Button label="Log In" className={loginBtnClass} onClick={loginSubmit} />
                     </div>
                   </div>
                 </div>
