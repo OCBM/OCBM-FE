9ed6d3ee2babe99327e15026545e635a6d36c52c
manoj <manoj.surabathulla@flyerssoft.com>
2023-10-12 20:30:59 +0530

    after login redirect to the plant screen

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -7,14 +7,14 @@ import { useBreadcrumbs } from '@/hooks';
 export function Layout() {
   const list = useBreadcrumbs();
   return (
-    <div className="flex h-screen py-[14px] pl-[14px] pr-[35px]">
+    <div className="flex h-screen py-[14px] pl-[14px]">
       <SideNav />
       <div className="px-[30px] w-full">
         <Header />
         <Breadcrumbs crumbs={['home', ...list]} />
-        <div>
+        <>
           <Outlet />
-        </div>
+        </>
       </div>
     </div>
   );
