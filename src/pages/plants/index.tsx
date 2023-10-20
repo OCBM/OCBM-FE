cde5935a401aa9be46a2b1273a28b4213f041902
harish-flyers <harishkumar.premkumar@flyerssoft.com>
2023-10-20 17:40:50 +0530

    small file changes

--- src/pages/plants/index.tsx
+++ src/pages/plants/index.tsx
@@ -16,7 +16,7 @@ function Plant() {
       setPlantData(res?.message);
     }
   };
-  const no_plant_container = classNames(
+  const plant_container = classNames(
     `w-full h-[76%] container mx-auto font-GothamMedium text-2xl font-medium shadow-2xl rounded-2xl p-6`,
   );
   const plantBtn = (val: any) => {
@@ -28,14 +28,14 @@ function Plant() {
   return (
     <>
       {plantData ? (
-        <div className={`${no_plant_container}`}>
+        <div className={`${plant_container}`}>
           <div className=" flex flex-col gap-6 w-full ">
-            <div className="py-5">
+            <div className="py-2">
               <h2 className="text-[#492CE1] font-GothamMedium text-2xl font-medium">
                 Can you tell us which plant are you looking at ?
               </h2>
             </div>
-            <div className="grid  grid-cols-5 gap-10 pr-6 h-[60vh] overflow-x-scroll ">
+            <div className="grid grid-cols-5 gap-10 pr-6 h-[60vh]">
               {plantData.map((data: any, i) => (
                 <div key={i}>
                   <PlantCard data={data} onClick={() => plantBtn(data)} />
