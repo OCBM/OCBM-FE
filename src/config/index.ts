6ed23713f2d7d4fac96a92d175c778bad03189fa
Raja rathinam v <raja.rathinam@flyerssoft.com>
2023-11-07 19:18:57 +0530

    config env

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,6 @@
 /// <reference types="vite/client" />
 
 export class Config {
-  static OMNEX_BACKEND_URL = import.meta.env.VITE_APP_API_BASE_URL;
+  static OMNEX_BACKEND_URL = '3.136.250.139';
+  // static OMNEX_BACKEND_URL = import.meta.env.VITE_APP_API_BASE_URL;
 }
