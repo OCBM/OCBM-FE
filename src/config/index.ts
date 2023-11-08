0a84609015c6143d76eb48b551d82829569ca0f1
Jay <jayashree.gunasekaran@flyerssoft.com>
2023-11-08 13:18:57 +0530

    backend URL change

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,4 @@
 export class Config {
-  static OMNEX_BACKEND_URL = 'http://13.233.20.37:4000/';
+  static OMNEX_BACKEND_URL = 'http://3.136.250.139:4000';
   //   static OMNEX_BACKEND_URL = import.meta.env.VITE_API_BASE_URL;
 }
