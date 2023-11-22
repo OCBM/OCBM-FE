0c2cb821b26a531c8b18327e8592a1539da442f1
manojflyer <manoj.surabathulla@flyerssoft.com>
2023-11-22 23:26:46 +0530

    removed commented line

--- src/components/reusable/loading/index.tsx
+++ src/components/reusable/loading/index.tsx
@@ -1,7 +1,6 @@
 const Loading = () => {
   return (
     <>
-      {/* <span className="absolute w-[30px] h-[30px] bg-white rotate-45 top-[-15px] left-[95px]"></span> */}
       <div className="flex gap-4">
         <div className="mb-5 px-2 rounded-2xl shadow-2xl w-[160px] h-[178px] flex flex-col items-center justify-between">
           <div className="flex animate-pulse flex-col items-center h-full justify-center p-3">
