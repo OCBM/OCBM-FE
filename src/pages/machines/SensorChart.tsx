b4ed7d19aff569ff9553762ed8b8b767e62a0957
yuva <yuva.prasath@flyerssoft.com>
2024-01-25 16:46:07 +0530

    Sensor Live Chart - Humidity and Temperature update

--- src/pages/machines/SensorChart.tsx
+++ src/pages/machines/SensorChart.tsx
@@ -7,14 +7,16 @@ import { getToken } from '@/lib/axios';
 Chart.register(zoomPlugin);
 const SensorChart: React.FC = () => {
   const chartRef = useRef<HTMLCanvasElement>(null);
+  const chartTempRef = useRef<HTMLCanvasElement>(null);
   const chartInstance = useRef<any>(null);
+  const chartTempFInstance = useRef<any>(null);
   const [, setSensorReadingsData] = useState<any>([]);
   const [sensorData, setSensorData] = useState<any>([]);
-  const date = new Date();
-  const hours = date.getHours();
-  const timeString = hours > 12 ? `${hours - 12}pm` : `${hours}am`;
-  var currentDate = new Date();
-  currentDate.setHours(currentDate.getHours() - 12);
+  // const date = new Date();
+  // const hours = date.getHours();
+  // const timeString = hours > 12 ? `${hours - 12}pm` : `${hours}am`;
+  // var currentDate = new Date();
+  // currentDate.setHours(currentDate.getHours() - 12);
 
   useEffect(() => {
     const token = getToken();
@@ -54,99 +56,116 @@ const SensorChart: React.FC = () => {
   }, []);
 
   console.log('sensorReadingsData', sensorData);
+  const dateFormat = (date: any) => {
+    const nDate = new Date(date);
+    let hours: number = nDate.getHours();
+    let ampm = hours >= 12 ? 'pm' : 'am';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    let strTime = hours + ' ' + ampm;
+    return strTime;
+  };
 
+  const chartConfig: any = (chartData: any) => {
+    return {
+      type: 'line',
+      data: chartData,
+      options: {
+        plugins: {
+          zoom: {
+            pan: {
+              enabled: true,
+              mode: 'x',
+            },
+            zoom: {
+              pinch: {
+                enabled: true,
+              },
+              wheel: {
+                enabled: true,
+              },
+              mode: 'x',
+            },
+          },
+        },
+        scales: {
+          x: {
+            min: 0,
+            max: 10,
+            grid: {
+              display: false,
+            },
+            border: {
+              color: 'transparent',
+            },
+          },
+          y: {
+            beginAtZero: true,
+            // min: item.min,
+            // max: item.max,
+            // ticks: {
+            //   stepSize: item.stepSize,
+            //   callback: (value: any) => value + ' ' + item.value,
+            //   color: (event: any) => {
+            //     return event?.tick?.value === item.min || event?.tick?.value === item.max ? 'green' : 'red';
+            //   },
+            // },
+
+            // Y Axis Min and Max value
+            suggestedMin: 0,
+            suggestedMax: 100,
+            ticks: {
+              maxTicksLimit: 6,
+              color: 'red',
+            },
+            border: {
+              dash: [10, 5],
+              color: '#A299D2',
+            },
+            grid: {
+              color: '#A299D2',
+            },
+          },
+          y1: {
+            // min: 0,
+            // max: 60,
+            // ticks: {
+            //   stepSize: chartData?.stepSize,
+            //   callback: (value: any) => value + ' ' + chartData?.value,
+            //   color: '#A299D2',
+            // },
+            ticks: {
+              color: '#A299D2',
+            },
+            position: 'right',
+            border: {
+              color: '#A299D2',
+            },
+            grid: {
+              color: '#A299D2',
+            },
+          },
+        },
+      },
+    };
+  };
   useEffect(() => {
     if (chartRef.current) {
       const ctx = chartRef.current.getContext('2d');
-
       if (ctx) {
-        chartInstance.current = new Chart(ctx, {
-          type: 'line',
-          data: {
-            labels: [],
-            datasets: [
-              {
-                label: 'Sensor Data',
-                data: [],
-                backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                borderColor: 'rgb(75, 192, 192)',
-                fill: false,
-              },
-            ],
-          },
-          options: {
-            plugins: {
-              zoom: {
-                pan: {
-                  enabled: true,
-                  mode: 'x',
-                },
-                zoom: {
-                  pinch: {
-                    enabled: true,
-                  },
-                  wheel: {
-                    enabled: true,
-                  },
-                  mode: 'x',
-                },
-              },
+        const chartData = {
+          labels: sensorData?.map((sensor: any) => dateFormat(sensor?.msgTimeStamp)),
+          datasets: [
+            {
+              label: 'Sensor Data',
+              data: sensorData?.map((sensor: any) => sensor?.humidity),
+              backgroundColor: 'rgba(75, 192, 192, 0.2)',
+              borderColor: 'rgb(75, 192, 192)',
+              fill: false,
             },
-            scales: {
-              x: {
-                min: 0,
-                max: 10,
-                grid: {
-                  display: false,
-                },
-                border: {
-                  color: 'transparent',
-                },
-              },
-              y: {
-                beginAtZero: true,
-                // min: item.min,
-                // max: item.max,
-                // ticks: {
-                //   stepSize: item.stepSize,
-                //   callback: (value: any) => value + ' ' + item.value,
-                //   color: (event: any) => {
-                //     return event?.tick?.value === item.min || event?.tick?.value === item.max ? 'green' : 'red';
-                //   },
-                // },
-                ticks: {
-                  color: 'red',
-                },
-                border: {
-                  dash: [10, 5],
-                  color: '#A299D2',
-                },
-                grid: {
-                  color: '#A299D2',
-                },
-              },
-              y1: {
-                // min: 0,
-                // max: 60,
-                // ticks: {
-                //   stepSize: chartData?.stepSize,
-                //   callback: (value: any) => value + ' ' + chartData?.value,
-                //   color: '#A299D2',
-                // },
-                ticks: {
-                  color: '#A299D2',
-                },
-                position: 'right',
-                border: {
-                  color: '#A299D2',
-                },
-                grid: {
-                  color: '#A299D2',
-                },
-              },
-            },
-          },
-        });
+          ],
+        };
+        chartInstance.current = new Chart(ctx, chartConfig(chartData));
         const scroller = (scroll: any, chart: any) => {
           console.log('object', scroll, chart);
         };
@@ -156,34 +175,75 @@ const SensorChart: React.FC = () => {
         });
 
         // Mock data update - Replace with actual data fetching logic
-        const interval = setInterval(() => {
-          const newData = Math.random() * 100; // Mock sensor data
-          const newLabel = timeString; // Mock time label
-          if (chartInstance.current) {
-            chartInstance.current.data.labels.push(newLabel);
-            chartInstance.current.data.datasets.forEach((dataset: any) => {
-              dataset.data.push(newData);
-            });
+        // const interval = setInterval(() => {
+        //   const newData = Math.random() * 100; // Mock sensor data
+        //   const newLabel = timeString; // Mock time label
+        //   if (chartInstance.current) {
+        //     chartInstance.current.data.labels.push(newLabel);
+        //     chartInstance.current.data.datasets.forEach((dataset: any) => {
+        //       dataset.data.push(newData);
+        //     });
 
-            // Update the maxTicks based on the length of labels
-            const maxTicks = Math.min(5, chartInstance.current.data.labels.length);
-            chartInstance.current.options.scales.x.maxTicks = maxTicks;
+        //     // Update the maxTicks based on the length of labels
+        //     const maxTicks = Math.min(5, chartInstance.current.data.labels.length);
+        //     chartInstance.current.options.scales.x.maxTicks = maxTicks;
 
-            chartInstance.current.update();
-          }
-        }, 1000);
+        //     chartInstance.current.update();
+        //   }
+        // }, 1000);
 
         return () => {
-          clearInterval(interval);
+          // clearInterval(interval);
           if (chartInstance.current) {
             chartInstance.current.destroy();
           }
         };
       }
     }
-  }, []);
+  }, [sensorData]);
 
-  return <canvas ref={chartRef}></canvas>;
+  useEffect(() => {
+    if (chartTempRef.current) {
+      const ctx = chartTempRef.current.getContext('2d');
+      if (ctx) {
+        const chartData = {
+          labels: sensorData?.map((sensor: any) => dateFormat(sensor?.msgTimeStamp)),
+          datasets: [
+            {
+              label: 'Sensor Data',
+              data: sensorData?.map((sensor: any) => sensor?.temperatureF),
+              backgroundColor: 'rgba(75, 192, 192, 0.2)',
+              borderColor: 'rgb(75, 192, 192)',
+              fill: false,
+            },
+          ],
+        };
+        chartTempFInstance.current = new Chart(ctx, chartConfig(chartData));
+        const scroller = (scroll: any, chart: any) => {
+          console.log('object', scroll, chart);
+        };
+
+        chartTempFInstance.current.canvas.addEventListener('wheel', (event: any) => {
+          scroller(event, chartTempFInstance.current);
+        });
+
+        return () => {
+          if (chartTempFInstance.current) {
+            chartTempFInstance.current.destroy();
+          }
+        };
+      }
+    }
+  }, [sensorData]);
+
+  return (
+    <>
+      <p className="uppercase text-[#444444] text-base font-medium ">Humidity</p>
+      <canvas ref={chartRef} />
+      <p className="uppercase text-[#444444] text-base font-medium mt-2">Temperature</p>
+      <canvas ref={chartTempRef} />
+    </>
+  );
 };
 
 export default SensorChart;
